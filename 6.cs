f55687c9833f26ba496826ca0b6e58094b6ce377
polinakolobova <55044590+polinakolobova@users.noreply.github.com>
2019-09-09 20:08:22 +0500

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        // задача 6 
+
+        static void Main()
+        {
+            int sumKv = 0; //сумма квадратов 
+            int kvSum = 0; //квадрат суммы 
+            for (var i = 1; i <= 100; i++)
+            {
+                sumKv = sumKv + i * i;
+                kvSum = kvSum + i;
+            }
+            kvSum = kvSum * kvSum;
+            Console.WriteLine(kvSum - sumKv);
+        }
+    }
+}
