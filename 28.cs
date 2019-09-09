04d8bcf49162722abf543f3b5d32210ea579c76c
polinakolobova <55044590+polinakolobova@users.noreply.github.com>
2019-09-09 22:28:32 +0500

    Add files via upload

---
+++ 28.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task28
+{
+    class Program
+    {
+        // задача 28 
+
+        static void Main()
+        {
+            int size = 1001;
+            int sum = 0;
+            int value = 1;
+            sum += value;
+            for (int i = 2; i < size; i += 2)
+                for (int j = 1; j <= 4; j++)
+                {
+                    value = value + i;
+                    sum += value;
+                }
+            Console.WriteLine(sum);
+        }
+    }
+}
